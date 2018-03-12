5dc61f76661b1b0612c0b1765b3d6f420ab3c9e7
Vignesh N <chiggy97.vn@gmail.com>
2018-03-12 19:20:04 +0530

    started parallel implementation

---
+++ parallel/header.h
@@ -0,0 +1,11 @@
+#include <mpi.h>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
