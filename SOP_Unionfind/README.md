621b624478c531928c0ec62707957fb82219feb5
VigneshN1997 <chiggy97.vn@gmail.com>
2018-08-29 12:46:10 +0530

    removed redundant files

--- SOP_Unionfind/README.md
+++
@@ -1,9 +0,0 @@
-# unionfind
-Simulating parallel implementations of union find sequentially to develop an optimized union find algorithm
-
-# to run:
-- modify parameters in main function in driver_mod.cpp
-
-g++ -std=c++11 driver.cpp
-
-// g++ -std=c++11 driver_mod.cpp is giving compilation error. Looking into it.
--- SOP_Unionfind/unionfind.h
+++
@@ -1,9 +0,0 @@
-#include "def.h"
-
-UnionFind* init_unionfind(long int n,int num_arrays);
-void random_initialize(UnionFind* uf,long int n,int num_arrays);
-void printUnionFind(UnionFind* uf);
-bool unify(long int x, long int y, UnionFind* uf,int process_of_x,int process_of_y,long int* num_messages);
-vector<Result*> do_statistical_analysis(vector <int> numBucketsArr,vector <long int> numPointsArr,vector<float> queryPercentArr);
-void unifyPathCompression(long int x, long int y, UnionFind* uf, int process_of_x, int process_of_y, long int* num_messages,long int* final_parent);
-void unifyPartialPathCompression(long int x, long int y, UnionFind* uf, int process_of_x, int process_of_y, long int* num_messages,long int* final_parent);
